9c019e96ee9c2ef48b81e0510d18d551d1f42647
Jeremy Lee <jrmylee@berkeley.edu>
2022-07-20 09:56:15 -0700

    add sidebar

---
+++ components/Sidebar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import {
+  Col,
+  Spacer,
+  Text,
+} from "@nextui-org/react";
+
+function Sidebar({elems, onClickItem}) {
+    const getItems = () => {
+        return elems.map((x) => {
+            return  ( 
+                <Text style={{cursor:'pointer'}} onClick={() => onClickItem(x)} size={20}>
+                    {x}
+                </Text>
+            )
+        });
+    }
+    return (
+        <Col css={{ w: '400px' }}>
+            <Spacer y={1} />
+            <Text size={20} weight={'bold'}>Cape Building ID Demo</Text>
+            <Spacer y={1} />
+            {getItems()}
+        </Col> 
+    );
+}
+
+export default Sidebar;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Sidebar from '../components/sidebar';
 import styles from '../styles/Home.module.css'
 import Map from './map/map';
 
 export default function Home() {
+  const items = ["2780 Glorietta Cir"];
+  const onClickItem = (x) => {
+
+  }
   return (
     <div className={styles.container}>
+      <Sidebar elems={items} onClickItem={onClickItem} />
       <Map />
     </div>
   )
