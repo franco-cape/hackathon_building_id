7067d9e1d969530d50a001b9f46f370e3b917da1
Franco Segura <franco.segura@capeanalytics.com>
2022-07-20 18:02:41 -0400

    added maps view - WIP

--- pages/buildings/[bid].js
+++ pages/buildings/[bid].js
@@ -1,5 +1,5 @@
 import styles from "./buildings.module.scss";
-import Map from "../map/map";
+import Map from "../../components/Map";
 import { useRouter } from 'next/router'
 import React, { useState, useEffect } from "react";
 import axios from "axios";
@@ -23,11 +23,15 @@ export default function Building() {
     console.log(buildingResult?.data?.data);
   }
 
-  const getMaps = () => buildings.map((b, idx) => <Map key={idx} elems={[b]} />)
+  const getMaps = () => buildings.map((b, idx) => <Map key={idx} building={b} id={b.cape_survey_id} />)
 
   return (
     <div className={styles.container}>
       {getMaps()}
+      {/* <Map key={1} building={buildings[0]} />
+      <Map key={2} building={buildings[1]} />
+      <Map key={3} building={buildings[2]} />
+      <Map key={4} building={buildings[3]} /> */}
     </div>
   );
 }
