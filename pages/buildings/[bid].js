9c4658284285abbfc40cd5960565683012ac44ea
Franco Segura <franco.segura@capeanalytics.com>
2022-07-22 17:41:40 -0400

    added boston api to fetch images

--- pages/buildings/[bid].js
+++ pages/buildings/[bid].js
@@ -1,6 +1,7 @@
 import styles from "./buildings.module.scss";
 import Map from "../map/map";
 import { useRouter } from "next/router";
+import Image from "next/image";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import DefaultTemplate from "../../components/Template";
@@ -11,12 +12,44 @@ function Building() {
     const [buildings, setBuildings] = useState([]);
     const [selectedBuilding, setSelectedBuilding] = useState(null);
 
+    const getGeochip = async (building) => {
+        const geo = JSON.parse(building.st_asgeojson);
+        const params = {
+            zoom: 21,
+            pad: 21,
+            algorithm: "geo_chipper",
+            survey: building.cape_survey_id,
+            geometry: {
+                type: geo.type,
+                coordinates: geo.coordinates,
+            },
+        };
+
+        const response = await axios.get(`http://localhost:3000/api/geochip`, {
+            params: params,
+        });
+
+        return { ...building, image: response.data.image };
+    };
+
     const getBuilding = async () => {
         const buildingResult = await axios.get(
             `http://localhost:3000/api/buildings/${bid}`
         );
-        setBuildings(buildingResult?.data?.data);
-        setSelectedBuilding(buildingResult?.data?.data[0]);
+        const buildingsArray = buildingResult?.data?.data;
+        setBuildings(buildingsArray);
+        setSelectedBuilding(buildingsArray[0]);
+
+        // geochip for all surveys
+        const images = buildingsArray.map(async (b) => {
+            const image = await getGeochip(b);
+
+            return image;
+        });
+
+        const buildingsWithImages = await Promise.all(images);
+
+        setBuildings(buildingsWithImages);
     };
 
     useEffect(() => {
@@ -40,21 +73,22 @@ function Building() {
     };
 
     const renderSurveys = () =>
-        buildings?.map((b, idx) => (
-            <div
-                key={idx}
-                data-buildingId={b.building_id}
-                className={`${styles.tile} ${
-                    b.cape_survey_id === selectedBuilding.cape_survey_id
-                        ? styles.active
-                        : ""
-                }`}
-                onClick={() => selectMap(idx)}
-            >
-                <p>Survey date</p>
-                <p>{formatDate(b.sv_image_date)}</p>
-            </div>
-        ));
+        buildings?.map((b, idx) => {
+            return (
+                <div
+                    key={idx}
+                    data-buildingId={b.building_id}
+                    className={`${styles.tile} ${
+                        b.cape_survey_id === selectedBuilding.cape_survey_id
+                            ? styles.active
+                            : ""
+                    }`}
+                    onClick={() => selectMap(idx)}
+                >
+                    <Image src={b.image} alt={b.sv_image_date} />
+                </div>
+            );
+        });
 
     const mapStyles = {
         height: "400px",
