cbe4338301a8efac96788ab49202860ae1ebafc7
franco-cape <107867955+franco-cape@users.noreply.github.com>
2022-07-26 21:13:01 -0400

    added constant coordinates, fixed animation (#7)

--- pages/map/map.js
+++ pages/map/map.js
@@ -1,17 +1,15 @@
 import mapboxgl from "mapbox-gl";
-import {useRef, useEffect} from "react";
+import { useRef, useEffect } from "react";
+import { coordinates } from "../../utils/contants";
 
-const Map = ({elems, customStyle}) => {
+const Map = ({ elems, customStyle }) => {
     const mapContainer = useRef(null);
     const map = useRef(null);
     const centroid = useRef(null);
 
     const addGeometries = (geoms) => {
         geoms.forEach((building) => {
-            if (
-                map.current && 
-                !map.current.getLayer(building.building_id)
-            ) {
+            if (map.current && !map.current.getLayer(building.building_id)) {
                 map.current.addSource(building.building_id, {
                     type: "geojson",
                     data: {
@@ -34,48 +32,53 @@ const Map = ({elems, customStyle}) => {
                     source: building.building_id, // reference the data source
                     type: "symbol",
                     layout: {
-                        'text-field': building.building_id,
-                        'text-size': 12
-                    }
-                })
+                        "text-field": building.building_id,
+                        "text-size": 12,
+                    },
+                });
             }
         });
-    }
+    };
 
     useEffect(() => {
         const loadFn = function () {
-            if(!centroid.current) return;
+            if (!centroid.current) return;
 
-            map.current.flyTo({center: centroid.current});
+            map.current.flyTo({ center: centroid.current });
             map.current.resize();
-            if(elems?.length > 0){
+            if (elems?.length > 0) {
                 addGeometries(elems);
             }
-        }
+        };
 
         mapboxgl.accessToken = process.env.MAPBOX_API_KEY ?? "";
         map.current = new mapboxgl.Map({
             container: mapContainer.current,
             style: "mapbox://styles/mapbox/light-v10",
-            zoom: 19,
+            center: [coordinates.lng, coordinates.lat],
+            zoom: 16,
         });
 
         map.current.on("style.load", loadFn);
         return () => {
             map.current.off("style.load", loadFn);
-        }
+        };
     }, []);
-    
+
     useEffect(() => {
-        if(elems?.length > 0 && elems[0]) {
-            let center = elems.length > 0 && JSON.parse(elems[0]?.centroid)?.coordinates || [-96.69771726896862, 32.91303284263627]
+        if (elems?.length > 0 && elems[0]) {
+            let center = (elems.length > 0 &&
+                JSON.parse(elems[0]?.centroid)?.coordinates) || [
+                coordinates.lng,
+                coordinates.lat,
+            ];
             centroid.current = center;
-            map.current.flyTo({center: center, zoom: 18});
+            map.current.flyTo({ center: center, zoom: 18 });
             addGeometries(elems);
         }
-    }, [elems])
+    }, [elems]);
 
-    return <div style={customStyle || MapStyles.map} ref={mapContainer}/>;
+    return <div style={customStyle || MapStyles.map} ref={mapContainer} />;
 };
 
 const MapStyles = {
